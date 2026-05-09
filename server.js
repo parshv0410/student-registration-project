73338fa41cc512e720a42c4ed5a52b7abb1f614c
parshv0410 <parshvpatel30@gmail.com>
2026-05-09 14:53:37 +0530

    My Full Stack Project

---
+++ server.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: "secretkey",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/studentDB");
+
+
+const userSchema = new mongoose.Schema({
+
+    name: String,
+    email: String,
+    password: String
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+
+app.get("/", (req, res) => {
+
+    res.render("index");
+
+});
+
+
+app.get("/login", (req, res) => {
+
+    res.render("login");
+
+});
+
+
+
+app.post("/update/:id", async (req, res) => {
+
+    await User.findByIdAndUpdate(req.params.id, {
+
+        name: req.body.name,
+        email: req.body.email
+
+    });
+
+    res.redirect("/dashboard");
+
+});
+
+
+
+app.post("/login", async (req, res) => {
+
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+
+    const foundUser = await User.findOne({ email: userEmail });
+
+    if(!foundUser){
+
+        return res.send("User Not Found");
+
+    }
+
+    const isMatch = await bcrypt.compare(
+        userPassword,
+        foundUser.password
+    );
+
+    if(isMatch){
+
+    req.session.user = foundUser;
+
+    res.redirect("/dashboard");
+
+    }
+    else{
+
+        res.send("Wrong Password");
+
+    }
+
+});
+    app.get("/edit/:id", async (req, res) => {
+
+    const user = await User.findById(req.params.id);
+
+    res.render("edit", { user });
+
+});
+    app.get("/delete/:id", async (req, res) => {
+
+    await User.findByIdAndDelete(req.params.id);
+
+    res.redirect("/dashboard");
+
+});
+app.listen(3000, () => {
+
+    console.log("Server Started");
+
+});
