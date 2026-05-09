5df5018d58e304ceada339aab141c9a35b2fb4e1
Your Name <youremail@gmail.com>
2026-05-09 16:00:31 +0530

    Final Fix

--- server.js
+++ server.js
@@ -19,9 +19,19 @@ app.use(session({
 }));
 
 
-mongoose.connect("mongodb+srv://parshvpatel30_db_user:<db_password>@cluster0.9wz1eja.mongodb.net/?appName=Cluster0");
+// MongoDB Atlas Connection
+mongoose.connect(
+    "mongodb+srv://parshvpatel30_db_user:Parshv@4356@cluster0.9wz1eja.mongodb.net/studentDB?retryWrites=true&w=majority"
+)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 
+// User Schema
 const userSchema = new mongoose.Schema({
 
     name: String,
@@ -34,6 +44,7 @@ const User = mongoose.model("User", userSchema);
 
 
 
+// Home Page
 app.get("/", (req, res) => {
 
     res.render("index");
@@ -41,6 +52,8 @@ app.get("/", (req, res) => {
 });
 
 
+
+// Login Page
 app.get("/login", (req, res) => {
 
     res.render("login");
@@ -49,6 +62,98 @@ app.get("/login", (req, res) => {
 
 
 
+// Dashboard
+app.get("/dashboard", async (req, res) => {
+
+    const users = await User.find();
+
+    res.render("dashboard", { users });
+
+});
+
+
+
+// Register User
+app.post("/register", async (req, res) => {
+
+    const userName = req.body.name;
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+
+    if(userPassword.length < 6){
+
+        return res.send("Password must be at least 6 characters");
+
+    }
+
+    const hashedPassword = await bcrypt.hash(userPassword, 10);
+
+    const newUser = new User({
+
+        name: userName,
+        email: userEmail,
+        password: hashedPassword
+
+    });
+
+    await newUser.save();
+
+    res.send("Registration Successful");
+
+});
+
+
+
+// Login User
+app.post("/login", async (req, res) => {
+
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+
+    const foundUser = await User.findOne({
+        email: userEmail
+    });
+
+    if(!foundUser){
+
+        return res.send("User Not Found");
+
+    }
+
+    const isMatch = await bcrypt.compare(
+        userPassword,
+        foundUser.password
+    );
+
+    if(isMatch){
+
+        req.session.user = foundUser;
+
+        res.redirect("/dashboard");
+
+    }
+    else{
+
+        res.send("Wrong Password");
+
+    }
+
+});
+
+
+
+// Edit Page
+app.get("/edit/:id", async (req, res) => {
+
+    const user = await User.findById(req.params.id);
+
+    res.render("edit", { user });
+
+});
+
+
+
+// Update User
 app.post("/update/:id", async (req, res) => {
 
     await User.findByIdAndUpdate(req.params.id, {
@@ -64,52 +169,29 @@ app.post("/update/:id", async (req, res) => {
 
 
 
-app.post("/login", async (req, res) => {
-
-    const userEmail = req.body.email;
-    const userPassword = req.body.password;
-
-    const foundUser = await User.findOne({ email: userEmail });
-
-    if(!foundUser){
-
-        return res.send("User Not Found");
-
-    }
-
-    const isMatch = await bcrypt.compare(
-        userPassword,
-        foundUser.password
-    );
-
-    if(isMatch){
-
-    req.session.user = foundUser;
-
-    res.redirect("/dashboard");
-
-    }
-    else{
-
-        res.send("Wrong Password");
-
-    }
-
-});
-    app.get("/edit/:id", async (req, res) => {
-
-    const user = await User.findById(req.params.id);
-
-    res.render("edit", { user });
-
-});
-    app.get("/delete/:id", async (req, res) => {
+// Delete User
+app.get("/delete/:id", async (req, res) => {
 
     await User.findByIdAndDelete(req.params.id);
 
     res.redirect("/dashboard");
 
 });
+
+
+
+// Logout
+app.get("/logout", (req, res) => {
+
+    req.session.destroy();
+
+    res.redirect("/login");
+
+});
+
+
+
+// Server
 app.listen(process.env.PORT || 3000, () => {
 
     console.log("Server Started");
